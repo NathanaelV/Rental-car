a2a43e34f69b0c06abbdb11efc8078ea45dd0a92
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-04-05 13:28:55 -0300

    Create table CAR_MODEL and insert values

---
+++ 04 - Car Brand criar tabela e inserir valores.sql
@@ -0,0 +1,21 @@
+-- SQLite
+
+-- Criar tabela
+CREATE TABLE CAR_BRAND (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    BRAND_NAME VARCHAR(100) NOT NULL
+);
+
+-- Inserir os valores
+INSERT INTO CAR_BRAND (BRAND_NAME) VALUES
+    ('Chevrolet'),
+    ('Toyota'),
+    ('Hyundai'),
+    ('Volkswagen'),
+    ('Jeep'),
+    ('Renault'),
+    ('Honda'),
+    ('Fiat');
+
+-- Visualizar
+SELECT * FROM CAR_BRAND
