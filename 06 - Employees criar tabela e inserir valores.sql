7817242fb5c622d212746d017b524ce4d3a39434
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-04-06 07:44:39 -0300

    Update datetime fields

--- 06 - Employees criar tabela e inserir valores.sql
+++ 06 - Employees criar tabela e inserir valores.sql
@@ -5,7 +5,7 @@ CREATE TABLE EMPLOYEES (
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
     NAME VARCHAR(200) NOT NULL,
     PHONE INTEGER NOT NULL,
-    CONTRACT_DATE TEXT,
+    CONTRACT_DATE DATETIME,
     POSITION_ID INTEGER,
 
     FOREIGN KEY (POSITION_ID) REFERENCES POSITIONS (ID)
@@ -13,12 +13,12 @@ CREATE TABLE EMPLOYEES (
 
 -- Inserir valores
 INSERT INTO EMPLOYEES (NAME, PHONE, CONTRACT_DATE, POSITION_ID) VALUES
-    ('Adriana Lemes', 	'(11) 98789-9999',	'10/04/2021',	1),
-    ('Camila Soares', 	'(11) 92749-9599',	'25/03/2008',	3),
-    ('Leonardo Silva', 	'(11) 91449-9600',	'26/08/2018',	5),
-    ('Mayara Gomes', 	'(11) 92649-9601',	'18/07/2016',	2),
-    ('Matheus Alves', 	'(11) 92749-9602',	'04/03/2015',	5),
-    ('Aline Santos', 	'(11) 99789-3135',	'06/05/2021',	3);
+    ('Adriana Lemes', 	'(11) 98789-9999',	'2021-04-10',	1),
+    ('Camila Soares', 	'(11) 92749-9599',	'2008-03-25',	3),
+    ('Leonardo Silva', 	'(11) 91449-9600',	'2018-08-26',	5),
+    ('Mayara Gomes', 	'(11) 92649-9601',	'2016-07-18',	2),
+    ('Matheus Alves', 	'(11) 92749-9602',	'2015-03-04',	5),
+    ('Aline Santos', 	'(11) 99789-3135',	'2021-05-06',	3);
 
 -- Mostrar valores
 SELECT * FROM EMPLOYEES;
